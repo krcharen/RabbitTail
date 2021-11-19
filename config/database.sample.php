33c0adc97b8007a5c75edeca6d4f1406831c3235
krcharen <krcharen@gmail.com>
2021-11-19 11:14:49 +0800

    add database sample file;

---
+++ config/database.sample.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'mysql' => [
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => '',
+        'username' => '',
+        'password' => '',
+        'unix_socket' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_general_ci',
+        'prefix' => '',
+    ],
+];
--- handle.php
+++ handle.php
@@ -2,15 +2,31 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$url = $_POST['url'];
+$url = trim($_POST['url']);
+$server = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
+
+$helper = new \App\Functions\Helper();
 
 $db = new \App\Functions\Database();
+
+//Check if the URL link exists.
+$exists = $db->rawSelect("SELECT `short_code` FROM `tail_link` WHERE `url` = '{$url}' AND `deleted_at` IS NULL ORDER BY `id` DESC;");
+
+if (!empty($exists)) {
+    $short_code = current($exists)['short_code'];
+    $short_url = $server . '/' . $short_code;
+
+    echo $helper::response(200, 'success', ['url' => $short_url]);
+
+    return true;
+}
+
+//Generate a new identification code.
 $next_id = $db->rawSelect("SELECT `value` FROM `tail_options` WHERE `item` = 'next_id';")[0]['value'];
 
 $core = new \App\Functions\Core();
 $short_code = $core->hexTo($next_id);
 
-$helper = new \App\Functions\Helper();
 $ip = $helper::client_ip();
 
 $insert = $update = 0;
@@ -24,10 +40,6 @@ if ($short_code) {
     }
 }
 
-$server = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
-
 $short_url = $insert && $update ? $server . '/' . $short_code : $server;
-
-$helper = new \App\Functions\Helper();
 
 echo $helper::response(200, 'success', ['url' => $short_url]);
