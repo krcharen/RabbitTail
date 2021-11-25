244c0185249af914a74dee77fed7103fafe34eb7
krcharen <krcharen@gmail.com>
2021-11-25 11:30:59 +0800

    fix

--- app/Handle/Loader.php
+++ app/Handle/Loader.php
@@ -3,6 +3,7 @@
 namespace App\Handle;
 
 use App\Functions\Database;
+use App\Functions\Helper;
 
 class Loader extends Database
 {
@@ -11,18 +12,51 @@ class Loader extends Database
     {
         $prase_uri = explode('/', trim($uri, '/'));
 
-        if (strlen($prase_uri[0]) > 7 || count($prase_uri) > 1 || strpos($prase_uri[0], '.') !== false) {
+        if (strlen($prase_uri[0]) > 8 || count($prase_uri) > 1 || strpos($prase_uri[0], '.') !== false) {
             Header("Location:/");
         }
 
         if (strlen($prase_uri[0]) === 8) {
             $short_code = trim($prase_uri[0]);
-            $url_sql = "SELECT `url` FROM `tail_links` WHERE `short_code` = '{$short_code}' AND `deleted_at` IS NULL;";
-            $select = $this->rawSelect($url_sql);
-            $url = current($select)['url'];
+            $url = $this->url($short_code);
             $redirect_to = $url ?: '/';
 
-            Header("Location: $redirect_to");
+            if (!headers_sent()) {
+                $protocol = $_SERVER['SERVER_PROTOCOL'];
+
+                if ('HTTP/1.1' != $protocol && 'HTTP/1.0' != $protocol) {
+                    $protocol = 'HTTP/1.0';
+                }
+
+                $status_desc = (new Helper())::get_http_status(302);
+
+                Header("$protocol 302 $status_desc");
+                Header("Location: $redirect_to");
+            }
+
+            if (PHP_SAPI !== 'cli') {
+                $this->redirect_by_javascript($redirect_to);
+            }
         }
+    }
+
+    /**
+     * @param string $code
+     * @return mixed
+     */
+    private function url(string $code = '')
+    {
+        $sql = "SELECT `url` FROM `tail_links` WHERE BINARY `short_code` = '{$code}' AND `deleted_at` IS NULL;";
+        $select = $this->rawSelect($sql);
+
+        return current($select)['url'];
+    }
+
+    /**
+     * @param string $redirect_to
+     */
+    private function redirect_by_javascript(string $redirect_to)
+    {
+        echo "<script type='text/javascript'>window.location='{$redirect_to}'</script>";
     }
 }
